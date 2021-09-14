ae61e652944fbbff79ea9b6bb2fdd2b6ac4da800
dominthebox <dsallustro@gmail.com>
2021-09-14 15:41:02 -0400

    wip

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -19,6 +19,18 @@ const restricted = (req, res, next) => {
 
     Put the decoded token in the req object, to make life easier for middlewares downstream!
   */
+  const token = req.headers.authorization
+    if (!token) {
+      return next({ status: 401, message: 'Token required' })
+    }
+    jwt.verify(token, JWT_SECRET, (err, decodedToken) => {
+      if (err) {
+        next({ status: 401, message: 'Token invalid' })
+      } else {
+        req.decodedToken = decodedToken
+        next()
+      }
+  })
 }
 
 const only = role_name => (req, res, next) => {
